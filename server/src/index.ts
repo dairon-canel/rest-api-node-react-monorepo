8eec3ff67ec8bca1d7720332940f3b5b90079bf8
Dairon Canel <98697567+dairon-canel@users.noreply.github.com>
2023-03-21 11:29:29 -0400

    fix: remove unuseful comments

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import http from 'http';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
@@ -33,62 +32,3 @@ app.listen(CONFIG.PORT, () => {
 initializeDB();
 
 app.use('/', router());
-
-/* 
-
-// Get a gateway by serial number
-app.get('/gateways/:serialNumber', async (req, res) => {
-  try {
-    const gateway = await GatewayModel.findOne({
-      serialNumber: req.params.serialNumber,
-    });
-    if (gateway) {
-      res.json(gateway);
-    } else {
-      res.status(404).json({ message: 'Gateway not found' });
-    }
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-// Add a peripheral device to a gateway
-app.post('/gateways/:serialNumber/peripheral-devices', async (req, res) => {
-  try {
-    const gateway = await GatewayModel.findOne({
-      serialNumber: req.params.serialNumber,
-    });
-    if (gateway) {
-      gateway.peripheralDevices.push(req.body);
-      const result = await gateway.save();
-      res.json(result);
-    } else {
-      res.status(404).json({ message: 'Gateway not found' });
-    }
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-});
-
-// Remove a peripheral device from a gateway
-app.delete(
-  '/gateways/:serialNumber/peripheral-devices/:uid',
-  async (req, res) => {
-    try {
-      const gateway = await GatewayModel.findOne({
-        serialNumber: req.params.serialNumber,
-      });
-      if (gateway) {
-        gateway.peripheralDevices = gateway.peripheralDevices.filter(
-          device => device.uid !== parseInt(req.params.uid),
-        );
-        const result = await gateway.save();
-        res.json(result);
-      } else {
-        res.status(404).json({ message: 'Gateway not found' });
-      }
-    } catch (error) {
-      res.status(400).json({ message: error.message });
-    }
-  },
-); */
