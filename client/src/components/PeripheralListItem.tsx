190483f9a04dabe6dd33f1a5d4c70729e39c8a79
Dairon Canel <98697567+dairon-canel@users.noreply.github.com>
2023-03-23 11:17:39 -0400

    fix: peripheral list items

--- client/src/components/PeripheralListItem.tsx
+++ client/src/components/PeripheralListItem.tsx
@@ -23,57 +23,57 @@ const PeripheralListItem: FC<IPeripheralListItemProps> = ({
     toggleEditClick,
   );
 
-  if (editButtonToggle) {
-    return (
-      <tr>
-        <td>{peripheral.uid}</td>
-        <td>**</td>
-        <td>
-          {peripheral.dateCreated
-            ? format(new Date(peripheral.dateCreated), 'yyyy/mm/dd')
-            : 'Not Available'}
-        </td>
-        <td>**</td>
-        <td>
-          <button
-            className="btn mt-1 min-h-[2rem] h-[2rem]"
-            onClick={() => handleEditToggle(peripheral)}
-            disabled={
-              (selectedPeripheral !== null &&
-                selectedPeripheral !== peripheral) ||
-              addAction
-            }
-          >
-            {selectedPeripheral === peripheral ? 'OK' : 'Edit'}
-          </button>
-        </td>
-      </tr>
-    );
-  }
-
   return (
     <tr>
-      <td>{peripheral.uid}</td>
-      <td>{peripheral.vendor}</td>
-      <td>
-        {peripheral.dateCreated
-          ? format(new Date(peripheral.dateCreated), 'yyyy/mm/dd')
-          : 'Not Available'}
-      </td>
-      <td>{peripheral.status}</td>
-      <td>
-        <button
-          className="btn mt-1 min-h-[2rem] h-[2rem]"
-          onClick={() => handleEditToggle(peripheral)}
-          disabled={
-            (selectedPeripheral !== null &&
-              selectedPeripheral !== peripheral) ||
-            addAction
-          }
-        >
-          {selectedPeripheral === peripheral ? 'OK' : 'Edit'}
-        </button>
-      </td>
+      {editButtonToggle ? (
+        <>
+          <td>{peripheral.uid}</td>
+          <td>**</td>
+          <td>
+            {peripheral.dateCreated
+              ? format(new Date(peripheral.dateCreated), 'yyyy/mm/dd')
+              : 'Not Available'}
+          </td>
+          <td>**</td>
+          <td>
+            <button
+              className="btn mt-1 min-h-[2rem] h-[2rem]"
+              onClick={() => handleEditToggle(peripheral, false)}
+              disabled={
+                (selectedPeripheral !== null &&
+                  selectedPeripheral !== peripheral) ||
+                addAction
+              }
+            >
+              {selectedPeripheral === peripheral ? 'OK' : 'Edit'}
+            </button>
+          </td>
+        </>
+      ) : (
+        <>
+          <td>{peripheral.uid}</td>
+          <td>{peripheral.vendor}</td>
+          <td>
+            {peripheral.dateCreated
+              ? format(new Date(peripheral.dateCreated), 'yyyy/mm/dd')
+              : 'Not Available'}
+          </td>
+          <td>{peripheral.status}</td>
+          <td>
+            <button
+              className="btn mt-1 min-h-[2rem] h-[2rem]"
+              onClick={() => handleEditToggle(peripheral, true)}
+              disabled={
+                (selectedPeripheral !== null &&
+                  selectedPeripheral !== peripheral) ||
+                addAction
+              }
+            >
+              {selectedPeripheral === peripheral ? 'OK' : 'Edit'}
+            </button>
+          </td>
+        </>
+      )}
     </tr>
   );
 };
