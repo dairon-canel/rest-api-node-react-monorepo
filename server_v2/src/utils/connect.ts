31705c5b7a440c7a8a126699eff2b9824cc78b74
Dairon Canel <98697567+dairon-canel@users.noreply.github.com>
2023-03-26 22:15:57 -0400

    feat: prepare deployment

--- server_v2/src/utils/connect.ts
+++ server_v2/src/utils/connect.ts
@@ -3,11 +3,17 @@ import config from 'config';
 import logger from './logger';
 
 async function connect() {
-  const dbUri = config.get<string>('dbUri');
-
   try {
+    let nodeEnv = 'production';
+    let dbUri = config.get<string>('dbUriProd');
+
+    if (config.get<string>('node_env') === 'test') {
+      dbUri = config.get<string>('dbUriDev');
+      nodeEnv = 'test';
+    }
+
     await mongoose.connect(dbUri);
-    logger.info('db connected');
+    logger.info(`Server connected to ${nodeEnv} environment`);
   } catch (error) {
     logger.error('Could not connect to db');
     process.exit(1);
